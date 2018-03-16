3378dec01ef63934b0e8e67e3a2d8c387c2e632c
Justin Davis <justinndavis@gmail.com>
2018-03-16 18:05:12 +0000

    sonarqube build

--- homework/sonarqube/deploy.sh
+++ homework/sonarqube/deploy.sh
@@ -7,6 +7,7 @@ oc new-app -f sonarqube-persistent-template.yml \
     -p APPLICATION_NAME=sonarqube \
     -p SOURCE_REPOSITORY_URL=https://github.com/justindav1s/ocp-appdev.git \
     -p SOURCE_REPOSITORY_URL=master \
+    -p DOCKERFILE_PATH="homework/sonarqube"
     -p SONARQUBE_JDBC_USERNAME=${DATABASE_USER} \
     -p SONARQUBE_JDBC_PASSWORD=${DATABASE_PASSWORD} \
     -p SONARQUBE_JDBC_URL=${DATABASE_URL}
