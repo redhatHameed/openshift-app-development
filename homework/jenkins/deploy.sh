0b9f67f3c8eaec9fc14f4416d3fc6e9f28408582
Justin Davis <justinndavis@gmail.com>
2018-03-16 21:16:36 +0000

    sonarqube build

---
+++ homework/jenkins/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+. ../env.sh
+
+oc login https://${IP}:8443 -u $USER
+
+oc delete project jenkins
+oc new-project jenkins 2> /dev/null
+while [ $? \> 0 ]; do
+    sleep 1
+    printf "."
+    oc new-project jenkins 2> /dev/null
+done
+
+oc new-app -f jenkins-persistent-template.yml \
+    -p DOMAIN=${DOMAIN} \
+    -p APPLICATION_NAME=jenkins \
+    -p SOURCE_REPOSITORY_URL=https://github.com/justindav1s/ocp-appdev.git \
+    -p SOURCE_REPOSITORY_URL=master \
+    -p DOCKERFILE_PATH="homework/jenkins" \
+    -p MEMORY_LIMIT=2Gi \
+    -p VOLUME_REQUEST=5Gi
