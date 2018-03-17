e9556bc92106d94c914322bfe835d6e7d9593035
Justin Davis <justinndavis@gmail.com>
2018-03-17 18:20:45 +0000

    jenkins build

---
+++ homework/jenkins_old/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+. ../env.sh
+
+oc delete all -l app=jenkins
+oc delete pvc jenkins-data
+oc delete serviceaccount jenkins
+
+oc new-app -f jenkins-persistent-template.yml \
+    -p DOMAIN=${DOMAIN} \
+    -p APPLICATION_NAME=jenkins \
+    -p JENKINS_VERSION=2 \
+    -p SOURCE_REPOSITORY_URL=https://github.com/justindav1s/ocp-appdev.git \
+    -p SOURCE_REPOSITORY_URL=master \
+    -p DOCKERFILE_PATH="homework/jenkins" \
+    -p MEMORY_LIMIT=2Gi \
+    -p VOLUME_REQUEST=5Gi
