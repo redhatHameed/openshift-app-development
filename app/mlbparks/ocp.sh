acb50a2018f4efa58edb401bbdc2365e8bfaf278
Justin Davis <justinndavis@gmail.com>
2018-03-18 19:43:57 +0000

    pipeline updates

--- app/mlbparks/ocp.sh
+++ app/mlbparks/ocp.sh
@@ -13,9 +13,9 @@ oc delete template ${APP}-dev-dc
 
 echo Setting up ${APP}
 oc new-build --binary=true --labels=app=${APP} --name=${APP} jboss-eap70-openshift:1.6 -n ${DEV_PROJECT}
-oc new-app ${DEV_PROJECT}/${APP}:0.0-0 --name=${APP} --allow-missing-imagestream-tags=true -n ${DEV_PROJECT}
-oc set triggers dc/${APP} --remove-all -n ${DEV_PROJECT}
-#oc new-app -f ${APP}-dev-dc.yaml --allow-missing-imagestream-tags=true -n ${DEV_PROJECT}
+#oc new-app ${DEV_PROJECT}/${APP}:0.0-0 --name=${APP} --allow-missing-imagestream-tags=true -n ${DEV_PROJECT}
+#oc set triggers dc/${APP} --remove-all -n ${DEV_PROJECT}
+oc new-app -f ${APP}-dev-dc.yaml --allow-missing-imagestream-tags=true -n ${DEV_PROJECT}
 oc expose dc ${APP} --port 8080 -n ${DEV_PROJECT}
 oc expose svc ${APP} -n ${DEV_PROJECT}
 
