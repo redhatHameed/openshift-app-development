291ee1222a3b7bdf82f7d53f8b77a0bcb2483cc8
Justin Davis <justinndavis@gmail.com>
2018-03-18 21:11:29 +0000

    pipeline updates

--- app/mlbparks/pipeline.groovy
+++ app/mlbparks/pipeline.groovy
@@ -14,6 +14,7 @@ node('maven') {
     def packaging    = getPackagingFromPom("pom.xml")
 
     stage('Checkout Source') {
+        echo giturl
         git credentialsId: 'gogs', url: git_url
     }
 
