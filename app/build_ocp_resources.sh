0d53349a9961b073783ddfc32891ae599f1ee0b7
Justin Davis <justinndavis@gmail.com>
2018-03-19 12:03:56 +0000

    pipeline updates

--- app/build_ocp_resources.sh
+++ app/build_ocp_resources.sh
@@ -16,7 +16,7 @@ oc delete configmap ${APP}-config -n ${DEV_PROJECT}
 
 echo Setting up ${APP} for ${DEV_PROJECT}
 oc new-build --binary=true --labels=app=${APP} --name=${APP} jboss-eap70-openshift:1.6 -n ${DEV_PROJECT}
-oc new-app -f ${APP}-dev-dc.yaml --allow-missing-imagestream-tags=true -n ${DEV_PROJECT}
+oc new-app -f ${APP}/${APP}-dev-dc.yaml --allow-missing-imagestream-tags=true -n ${DEV_PROJECT}
 oc set volume dc/${APP} --add --name=${APP}-config-vol --mount-path=/opt/eap/standalone/configuration/${APP}-config --configmap-name=${APP}-config -n ${DEV_PROJECT}
 oc expose dc ${APP} --port 8080 -n ${DEV_PROJECT}
 oc expose svc ${APP} -n ${DEV_PROJECT}
@@ -29,14 +29,14 @@ oc delete configmap ${APP}-${COLOUR}-config -n ${PROD_PROJECT}
 echo Setting up ${APP} for ${PROD_PROJECT}
 COLOUR=blue
 oc new-build --binary=true --labels=app=${APP} --name=${APP}-${COLOUR} jboss-eap70-openshift:1.6 -n ${PROD_PROJECT}
-oc new-app -f ${APP}-prod-dc.yaml --allow-missing-imagestream-tags=true -p BLUE_OR_GREEN=${COLOUR} -n ${PROD_PROJECT}
+oc new-app -f ${APP}/${APP}-prod-dc.yaml --allow-missing-imagestream-tags=true -p BLUE_OR_GREEN=${COLOUR} -n ${PROD_PROJECT}
 oc set volume dc/${APP}-${COLOUR} --add --name=${APP}-${COLOUR}-config-vol --mount-path=/opt/eap/standalone/configuration/${APP}-config --configmap-name=${APP}-${COLOUR}-config -n ${PROD_PROJECT}
 oc expose dc ${APP}-${COLOUR} --port 8080 -n ${PROD_PROJECT}
 oc expose svc ${APP}-${COLOUR} -n ${PROD_PROJECT}
 
 COLOUR=green
 oc new-build --binary=true --labels=app=${APP} --name=${APP}-${COLOUR} jboss-eap70-openshift:1.6 -n ${PROD_PROJECT}
-oc new-app -f ${APP}-prod-dc.yaml --allow-missing-imagestream-tags=true -p BLUE_OR_GREEN=${COLOUR} -n ${PROD_PROJECT}
+oc new-app -f ${APP}/${APP}-prod-dc.yaml --allow-missing-imagestream-tags=true -p BLUE_OR_GREEN=${COLOUR} -n ${PROD_PROJECT}
 oc set volume dc/${APP}-${COLOUR} --add --name=${APP}-${COLOUR}-config-vol --mount-path=/opt/eap/standalone/configuration/${APP}-config --configmap-name=${APP}-${COLOUR}-config -n ${PROD_PROJECT}
 oc expose dc ${APP}-${COLOUR} --port 8080 -n ${PROD_PROJECT}
 oc expose svc ${APP}-${COLOUR} -n ${PROD_PROJECT}
